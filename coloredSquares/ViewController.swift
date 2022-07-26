4145252aecd2e9d976c008ad336e6b2ff742013b
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2022-07-26 13:45:45 +0300

    Initial Commit

---
+++ coloredSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coloredSquares
+//
+//  Created by Anton Akhmedzyanov on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
